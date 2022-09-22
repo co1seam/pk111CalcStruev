b3288aa74887646ad41ec7b2fbc5e9ff8f94483f
Constantine Struev <guystruev@gmail.com>
2022-09-22 15:04:10 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StruevCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            var a = (Button)sender;
+            textBox1.Text += a.Text;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length>24)
+            {
+                string txt = textBox1.Text;
+                textBox1.Text = txt.Substring(0, 24);
+
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string txt = textBox1.Text;
+            textBox1.Text = txt.Substring(0, textBox1.Text.Length-1);
+        }
+    }
+}
